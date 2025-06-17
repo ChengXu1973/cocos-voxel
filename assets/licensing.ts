822fb8fa154dc85b08e84b5b688cbee20232a5c2
chengxu1973 <chengxu1973@outlook.com>
2025-06-17 21:56:32 +0800

    dist

--- assets/licensing.ts
+++
@@ -1,9 +0,0 @@
-import { _decorator, Component } from "cc";
-const { ccclass, property } = _decorator;
-
-@ccclass("licensing")
-export class licensing extends Component {
-    openLink(_, url: string) {
-        window.open(url, "_blank");
-    }
-}
